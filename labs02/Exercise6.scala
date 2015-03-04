c78560d716e61557d2b2e3d86bb369a3551b71f5
Maelle Colussi <maellecolussi@yahoo.fr>
2015-03-04 16:16:21 +0100

    correct var to val, simplify rest

--- labs02/Exercise6.scala
+++ labs02/Exercise6.scala
@@ -1,8 +1,3 @@
-//first part ok perhaps
-//+ and contains ok perhaps
-//-, toList : ask question
-//++ : also ask question
-
 import leon.lang._
 import leon.collection._
 import leon._
@@ -52,7 +47,7 @@ object BinaryTree {
              res.size >= this.size &&
              res.size <= this.size + 1
     }
- 
+
     def -(x: BigInt): Tree = {
       require(isBT)
       this match {
@@ -61,10 +56,7 @@ object BinaryTree {
         if (x == v) {
           if (l.size == 0) r 
           else if (r.size == 0) l
-          else {
-            var y1 = l.rightElt().get
-            Node(l - y1, y1, r)
-          }
+          else l ++ r
         }
         else if (x < v) Node(l - x, v, r)
         else Node(l, v, r - x)
@@ -75,46 +67,19 @@ object BinaryTree {
              res.size <= this.size &&
              res.size >= this.size - 1
     }
-    
-    // 
-    def rightElt() : Option[BigInt] = {
-      require(isBT)
-      this match {
-        case Empty => None[BigInt]
-        case Node(l, v, r) => 
-        if(r.size == 0) Some(v)
-        else r.rightElt()
-      }
-    } ensuring { res => (!res.isDefined || this.contains(res.get)) && (this.size == 0 || res.isDefined)}
-    
  
     def ++(that: Tree): Tree = {
       require(this.isBT && that.isBT)
-      
-      def listToTree(l: List[BigInt], buf : Tree) : Tree = {
-        require(isSorted(l.reverse))
-        l match {
-          case Nil() => buf
-          case Cons(h, t) => listToTree(t, Node(Empty, h, buf))
+      this match {
+        case Empty => that
+        case Node(li, vi, ri) => that match {
+          case Empty => this
+          case Node(la, va, ra) => 
+            if(vi == va) Node(li ++ la, vi, ri ++ ra)
+            else if (vi < va) la ++ Node(li, vi, (ri + va) ++ ra)
+            else Node((li + va) ++ la, vi, ri) ++ ra
         }
-      } ensuring{_.isBT}
-      
-      def mergeLists(l1: List[BigInt], l2: List[BigInt], buf: List[BigInt]) : List[BigInt] = {
-        require(isSorted(l1) && isSorted(l2))
-        if(l1.isEmpty) l2.reverse ++ buf
-        else if (l2.isEmpty) l1.reverse ++ buf
-        else if (l1.head < l2.head){
-          if (!buf.isEmpty && l1.head == buf.head) mergeLists(l1.tail, l2, buf)
-          else mergeLists(l1.tail, l2, List(l1.head) ++ buf)
-        }
-        else if (l1.head == l2.head) mergeLists(l1.tail, l2, buf)
-        else {
-          if (!buf.isEmpty && l2.head == buf.head) mergeLists(l1, l2.tail, buf)
-          else mergeLists(l1, l2.tail, List(l2.head) ++ buf)
-        }
-      } ensuring{res => res.size <= l1.size + l2.size && res.content == l1.content ++ l2.content && isSorted(res.reverse)}
-      
-      listToTree(mergeLists(this.toList, that.toList, Nil()), Empty)// TODO
+      }//TODO
     } ensuring {
       res => res.isBT && res.content == this.content ++ that.content
     }

21dff300528608cb6e4a4990d98b8a4c8277d0f2
Mathieu Demarne <mathieu.demarne@gmail.com>
2015-03-25 15:55:11 +0100

    Cleaning up Queue: Scala/Leon handle exceptions

--- catenable-lists/src/main/scala/Queue.scala
+++ catenable-lists/src/main/scala/Queue.scala
@@ -1,30 +1,39 @@
 /* 
  * Implementation of Queue based on "Purely Functionnal Data Structure, Okasaki, P15+" 
- * */
+ * @author Maëlle Colussi
+ * @author Mathieu Demarne
+ */
 
-case class Queue[T](f : List[T], r: List[T]){
+object Queue {
+
+	val empty = Queue(Nil, Nil)
+
+}
+
+case class Queue[T](f : List[T], r: List[T]) {
 
 	def isEmpty = f.isEmpty && r.isEmpty
 
-	def queueInv(f : List[T], r: List[T]): Queue[T] = {
-		if (f.isEmpty) Queue(r.reverse, Nil)
-		else Queue(f, r)
+	/* TODO: use for invariant checking or remove ? */
+	def queueInv(f : List[T], r: List[T]): Queue[T] = f match {
+		case Nil => Queue(r.reverse, Nil)
+		case _ => Queue(f, r)
 	}
-	def head: T = { //put require non empty
-		f match {
-			case x :: xs => x 
-			case Nil => sys.error("") //not possible if invariant holds
+
+	def head: T = f.head //put require non empty
+	
+	def tail: Queue[T] = { // put require non empty
+		f.tail match {
+			case Nil => Queue(r.reverse, Nil)
+			case xs => Queue(xs, r)
 		}
 	}
 	
-	def tail: Queue[T] = {
-		f match {
-			case x :: xs => Queue(xs, r)
-			case Nil => sys.error("")
-		}
+	def snoc(x: T): Queue[T] = f match {
+		case Nil => Queue(x :: Nil, Nil)
+		case _ =>  Queue(f, x :: r)
 	}
-	
-	def snoc(x: T): Queue[T] = Queue(f, x :: r)
+
 	//TODO : change to leon's lists
 	//TODO : invariant
 }

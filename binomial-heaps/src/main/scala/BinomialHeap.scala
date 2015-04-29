004bf0a02a0ccdf661df0d71faed09ab0c46a3b7
Maelle Colussi <maellecolussi@yahoo.fr>
2015-04-29 16:57:39 +0200

    more BinomialHeap and Tree

--- binomial-heaps/src/main/scala/BinomialHeap.scala
+++ binomial-heaps/src/main/scala/BinomialHeap.scala
@@ -18,8 +18,27 @@ sealed abstract class BinomialHeap[T] {
 	def isDefined: Boolean = !this.isEmpty
 	//def size: BigInt = {???} ensuring (res => res == this.toList.size && res >= 0)
 
-	def insert(x: T): BinomialHeap[T] = {???}
-	def merge(that: BinomialHeap[T]): BinomialHeap[T] = {???}
+	def insert(x: T): BinomialHeap[T] = this.insTree(Node(0, x, Nil()))
+	def insTree(that: Tree[N]): BinomialHeap[T] = {//t1: that, ts: this
+		this match {
+			case Cons(t2, rest) => {
+				if (that.rank < t2.rank) that :: this
+				else rest.insTree(that.link(t2))
+			}
+			case Nil() => Cons(that, Nil())
+		}
+	}
+	def merge(that: BinomialHeap[T]): BinomialHeap[T] = {
+		(this, that) match {
+			case (t, Nil()) => t
+			case (Nil(), t) => t
+			case (t1 :: ts1, t2 :: ts2) => {
+				if (t1.rank < t2.rank)  t1 :: ts1.merge(that)
+				else if (t2.rank > t1.rank) t2 :: this.merge(ts2)
+				else merge(ts1, ts2).insTree(t1.link(t2))
+			}
+		}
+	}
 	def findMin(): T = {???}
 	def deleteMin(): BinomialHeap[T] = {???} 
 	
--- binomial-heaps/src/main/scala/Tree.scala
+++ binomial-heaps/src/main/scala/Tree.scala
@@ -9,10 +9,22 @@ import leon.collection._
  * @author Mathieu Demarne
  */
 
+//TODO : how to write in Scala that we want such an object to exist for T ?
+object TOrdering[T] extends Ordering[T] {
+  def compare(a:T, b:T) = ...
+}
 
 sealed abstract class Tree[T] {
 	def link(that: Tree[T]) : Tree[T] = {
-		
+		(this, that) match {
+			case (TreeNode[T](r, x1, c1), TreeNode[T](_, x2, c2)) =>
+				if (TOrdering.lteq(x1, x2)) TreeNode[T](r + 1, x2, that :: c1)
+				else TreeNode[T](r + 1, x2, this :: c2)
+		}
+	}
+	
+	def rank(): BigInt = this match {
+		case TreeNode[T](r, x, c) => r
 	}
 }
 
